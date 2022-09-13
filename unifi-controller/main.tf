95f08bc3d32c826d11b8b44cec7cc06bee6b9c29
JayDee Raymaker <16980904+jd-raymaker@users.noreply.github.com>
2022-09-13 13:26:50 +0200

    refactoring

--- unifi-controller/main.tf
+++ unifi-controller/main.tf
@@ -21,49 +21,28 @@ resource "docker_container" "unifi_container" {
   name    = "unifi-controller"
   restart = "unless-stopped"
   env     = ["MEM_LIMIT=1024", "MEM_STARTUP=1024"]
+
   volumes {
     volume_name    = "unifi_config"
     container_path = "/config"
   }
-  ports {
-    internal = 8443
-    external = 8443
+
+  dynamic "ports" {
+    for_each = var.tcp_ports
+    content {
+      internal = ports.value
+      external = ports.value
+      protocol = "tcp"
+    }
   }
-  ports {
-    internal = 3478
-    external = 3478
-    protocol = "udp"
-  }
-  ports {
-    internal = 10001
-    external = 10001
-    protocol = "udp"
-  }
-  ports {
-    internal = 8080
-    external = 8080
-  }
-  ports {
-    internal = 1900
-    external = 1900
-    protocol = "udp"
-  }
-  ports {
-    internal = 8843
-    external = 8843
-  }
-  ports {
-    internal = 8880
-    external = 8880
-  }
-  ports {
-    internal = 6789
-    external = 6789
-  }
-  ports {
-    internal = 5514
-    external = 5514
-    protocol = "udp"
+
+  dynamic "ports" {
+    for_each = var.udp_ports
+    content {
+      internal = ports.value
+      external = ports.value
+      protocol = "udp"
+    }
   }
 }
 
--- unifi-controller/variables.tf
+++ unifi-controller/variables.tf
@@ -2,3 +2,15 @@ variable "docker_host" {
   type        = string
   description = "Docker host address"
 }
+
+variable "tcp_ports" {
+  type        = list(number)
+  description = "List of TCP ports"
+  default     = [8443, 8080, 8843, 8880, 6789]
+}
+
+variable "udp_ports" {
+  type        = list(number)
+  description = "List of UDP ports"
+  default     = [3478, 10001, 1900, 5514]
+}
